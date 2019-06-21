de4185d7b010786c39fe08f963ff2ddeb4c3214d
hanmiaomiao <18531151201@163.com>
2019-06-21 17:43:20 +0800

    '创建组件并配置路由-用户登录'

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+   {
+    name:'home',
+    path:'/',
+    //在整个项目里，模块路径中 @ 表示的是 src 目录
+    // 无论你当前模块在哪里， @ 都可以直接点位到src
+    // 加载一个目录就可以默认加载他的index.js\indexedDB.vue\indexedDB.son
+    component:()=> import('@views/home')
+   },
+   {name:'login',
+    path:'/login',
+    component:()=>import('@views/login')
+   
+  }
    
   ]
 })
---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <router-view/>
+</template>
+
+<script>
+export default {
+    name:'AppLogin',
+    data(){
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
