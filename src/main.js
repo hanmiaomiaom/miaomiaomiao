109c4c8cb09cad6206d7cb0f1a58ef580f78f46f
hanmiaomiao <18531151201@163.com>
2019-06-29 19:12:27 +0800

    文章列表-组件布局

--- src/main.js
+++ src/main.js
@@ -8,16 +8,16 @@ import axios from 'axios'
 // 如果找到目录，优先加载目录中的index
 import router from './router'
 
-// 所有组件都是Vue实例，我们可以把一些需要在组件中频繁使用的成员放到Vue.prototype中
-// 给Vue原型对象扩展成员时，最好加上一个$前缀，避免和组件内部的数据成员冲突
-Vue.prototype.$http = axios
+// 引入公共样式文件，最好在element 样式文件之后，可以自定义修改elment 内置样式
+import './styles/index.less'
 
 // 配置 axios 的基础路径，发送请求时就不需要每次再写http://xxx
 // 路径后边的 /.  多退少补
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+// 所有组件都是Vue实例，我们可以把一些需要在组件中频繁使用的成员放到Vue.prototype中
+// 给Vue原型对象扩展成员时，最好加上一个$前缀，避免和组件内部的数据成员冲突
+Vue.prototype.$http = axios
 
-// 引入公共样式文件，最好在element 样式文件之后，可以自定义修改elment 内置样式
-import './styles/index.less'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
---
+++ src/views/article/index.vue
@@ -0,0 +1,88 @@
+<template>
+    <div>
+        <!-- 数据筛选 -->
+        <el-card class="filter-card">
+            <div slot="header" class="clearFix">
+                <span>数据筛选</span>
+                <el-button style="float:right; padding:3px 0" type="text">按钮操作</el-button>
+            </div>
+            <div v-for="o in 4" :key="o" class="text item">
+                 {{'列表内容 ' + o }}
+            </div>
+</el-card>
+<!-- 数据筛选 -->
+
+<!-- 文章列表 -->
+<el-card class="box-card">
+  <div slot="header" class="clearfix">
+    <span>一共有xxx条数据</span>
+    <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+  </div>
+ <el-table
+    class="article-list"
+    :data="tableData"
+    style="width:100%"
+    >
+    <el-table-column
+      label="日期"
+      prop="date"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      label="名字"
+      prop="name"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      label="地址"
+      prop="address">
+    </el-table-column>
+      </el-table>
+      <!-- 数据分页 -->
+  <el-pagination
+    layout="prev, pager, next"
+    :total="1000">
+  </el-pagination>
+</el-card>
+<!-- 文章列表 -->
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'ArticleList',
+    data () {
+        return {
+            tableData:[{
+                date:'2016-01-04',
+                name:'程小二',
+                address:'上海市普陀区金沙江路1518栋'
+         },
+         {
+             date: '2016-05-04',
+             name: '王小虎',
+             address: '上海市普陀区金沙江路 1517 栋' 
+         },
+         {
+              date: '2016-05-06',
+             name: '王小哇',
+             address: '上海市普陀区金沙江路 1516 栋' 
+         }]
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.fliter-card {
+    margin-bottom: 15px;
+}
+.el-card {
+    text-align: left;
+}
+.article-list {
+    margin-bottom: 30px;
+}
+</style>
+
