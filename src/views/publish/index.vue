70d54d7e50fe50b7ceae54557b1b5d743fa9f4d6
hanmiaomiao <18531151201@163.com>
2019-07-01 20:08:09 +0800

    发布文章-布局

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,17 +1,67 @@
 <template>
-  <div>
-    <p>Public Component</p>
+  <el-card class="publish-card">
+  <div slot="header" class="header">
+    <span>发表文章</span>
+    <div>
+      <el-button >发表</el-button>
+      <el-button >存入草稿</el-button>
+    </div>
   </div>
+  <!-- 栅格布局 -->
+  <el-row >
+    <el-col :span="10">
+          <el-form ref="form" :model="articleForm" label-width="80px">
+        <el-form-item label="标题">
+          <el-input v-model="articleForm.title"></el-input>
+        </el-form-item>
+        <el-form-item label="内容">
+          <el-input type="textarea" v-model="articleForm.content"></el-input>
+        </el-form-item>
+        <el-form-item label="封面">
+          
+        </el-form-item>  
+      <el-form-item label="频道">
+        <el-select v-model="articleForm.channel_id" placeholder="请选择活动区域">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+    <!-- 表单 -->
+    
+  <!-- 表单 -->
+</el-card>
 </template>
 
 <script>
 export default {
   name: 'AppPublish',
   data() {
-    return {}
+    return {
+      articleForm: {
+          title: '',//标题
+          content: '',//内容
+         channel_id:'3',//频道
+         cover:{//封面
+         type:0,//封面类型  -1：自动，0- 无图 1-1张 3-3张
+         images:[]
+         }
+        }
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.publish-card {
+  min-height: 100%;
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+}
 </style>
